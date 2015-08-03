4a7c2b1688ce8d460a88a53f66400feefe55dc84
Mike Gallay <mikegallay@gmail.com>
2015-08-03 16:30:02 -0400

    proper sync

---
+++ js/dynamic-sheen.js
@@ -0,0 +1,17 @@
+var DynamicSheen = {
+
+	type : 'text', // 'image'/'text'
+	shadow: false, //tf
+	
+
+	init : function(){
+		var _scope = this;
+		
+		_scope.addHandlers();
+		
+	},
+
+	addHandlers : function(){
+	
+	}
+}
